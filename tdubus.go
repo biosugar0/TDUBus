4f67ba2f58c971bb7d06b95413054b96a6e24038
Yuto Kimura <biostudy28@gmail.com>
2018-01-31 19:42:19 +0900

    update

--- tdubus.go
+++ tdubus.go
@@ -6,6 +6,17 @@ import (
 	"strings"
 )
 
+type BusTimes struct {
+	Up   Schedule
+	Down Schedule
+}
+type Schedule struct {
+	Takasaka   map[string][]string
+	Kitasakado map[string][]string
+	Kumagaya   map[string][]string
+	Kounosu    map[string][]string
+}
+
 func GetTimeSchedules() []BusTimes {
 	url := "https://www.dendai.ac.jp/access/saitama_hatoyama/school_bus.html"
 	doc, err := goquery.NewDocument(url)
@@ -43,17 +54,6 @@ func getschedule(doc *goquery.Document) []Schedule {
 	return result
 }
 
-type BusTimes struct {
-	Up   Schedule
-	Down Schedule
-}
-type Schedule struct {
-	Takasaka   map[string][]string
-	Kitasakado map[string][]string
-	Kumagaya   map[string][]string
-	Kounosu    map[string][]string
-}
-
 func getTime(data *goquery.Selection) Schedule {
 	ret := Schedule{}
 	ret.Takasaka = make(map[string][]string)
@@ -65,10 +65,10 @@ func getTime(data *goquery.Selection) Schedule {
 	d.Each(func(n int, s *goquery.Selection) {
 		hour := strings.TrimSpace(s.Find("th").Text())
 		mbase := s.Find("td")
-		ret.Takasaka[hour] = makeSchedule(mbase.Slice(0, 1))
-		ret.Kitasakado[hour] = makeSchedule(mbase.Slice(1, 2))
-		ret.Kumagaya[hour] = makeSchedule(mbase.Slice(2, 3))
-		ret.Kounosu[hour] = makeSchedule(mbase.Slice(3, 4))
+		ret.Takasaka[hour] = timesplit(mbase.Slice(0, 1))
+		ret.Kitasakado[hour] = timesplit(mbase.Slice(1, 2))
+		ret.Kumagaya[hour] = timesplit(mbase.Slice(2, 3))
+		ret.Kounosu[hour] = timesplit(mbase.Slice(3, 4))
 	})
 	return ret
 }
@@ -88,17 +88,21 @@ func getTimeholiday(data *goquery.Selection) (Schedule, Schedule) {
 		if n > 0 {
 			hour := s.Find("th").Text()
 			mbase := s.Find("td")
-			ret1.Kumagaya[hour] = makeSchedule(mbase.Slice(0, 1))
-			ret1.Kitasakado[hour] = makeSchedule(mbase.Slice(1, 2))
-			ret1.Takasaka[hour] = makeSchedule(mbase.Slice(2, 3))
-			ret2.Takasaka[hour] = makeSchedule(mbase.Slice(3, 4))
-			ret2.Kitasakado[hour] = makeSchedule(mbase.Slice(4, 5))
-			ret2.Kumagaya[hour] = makeSchedule(mbase.Slice(5, 6))
+			ret1.Kumagaya[hour] = timesplit(getvalue(mbase, 0))
+			ret1.Kitasakado[hour] = timesplit(getvalue(mbase, 1))
+			ret1.Takasaka[hour] = timesplit(getvalue(mbase, 2))
+			ret2.Takasaka[hour] = timesplit(getvalue(mbase, 3))
+			ret2.Kitasakado[hour] = timesplit(getvalue(mbase, 4))
+			ret2.Kumagaya[hour] = timesplit(getvalue(mbase, 5))
 		}
 	})
 	return ret1, ret2
 }
-func makeSchedule(data *goquery.Selection) []string {
+
+func getvalue(data *goquery.Selection, n int) *goquery.Selection {
+	return data.Slice(n, n+1)
+}
+func timesplit(data *goquery.Selection) []string {
 	ret := []string{}
 	d := strings.TrimSpace(data.Text())
 	ret = strings.Fields(d)
